ecdd7a7f2908e4ada9befd1799e029e0a2a237a9
NastyaMedv <NastyaMedv@gmail.com>
2019-05-27 00:55:14 +0300

    reel set

--- src/slot.ts
+++ src/slot.ts
@@ -1,28 +1,49 @@
 import * as PIXI from 'pixi.js';
 
-var renderer = PIXI.autoDetectRenderer(1200, 900, {backgroundColor: 0x1099bb});
+var renderer = PIXI.autoDetectRenderer(1220, 900, {backgroundColor: 0x1099bb});
 document.body.appendChild(renderer.view);
 
-const startX = 100;
+const startX = 10;
 const startY = 10;
+const width = 240;
+const height = 230;
 
 var stage = new PIXI.Container();
-var texture = PIXI.Texture.fromImage('img/symbol_2.png');
-var symbol = new PIXI.Sprite(texture);
+var reel = new PIXI.Container();
+stage.addChild(reel)
 
-symbol.anchor.x = 0;
-symbol.anchor.y = 0;
-symbol.position.x = startX;
-symbol.position.y = startY;
-symbol.scale.x = 1;
-symbol.scale.y = 1;
-stage.addChild(symbol);
+var myMask = new PIXI.Graphics();
+myMask.beginFill();
+myMask.drawRect(20, 20, 1200, 700);
+myMask.endFill();
+stage.addChild(myMask);
+reel.mask = myMask;
+
+initReel();
+
+function initReel() {
+  for (let i=0; i<5; i++) {
+    for (let j=0; j<3; j++) {
+      let picId = Math.floor(Math.random()*6+1);
+      let texture = PIXI.Texture.fromImage('img/symbol_'+picId+'.png');
+      let symbol = new PIXI.Sprite(texture);
+      symbol.anchor.x = 0;
+      symbol.anchor.y = 0;
+      symbol.position.x = startX + i * width;
+      symbol.position.y = startY + j * height;
+      symbol.scale.x = 1;
+      symbol.scale.y = 1;
+      reel.addChild(symbol);
+      //animate();
+    }
+  }
+}
+
 animate();
-
 function animate() {
     requestAnimationFrame(animate);
-    var symbol = stage.getChildAt(0);
-    symbol.y += 3;
-    renderer.render(stage);
-    if (symbol.y>600) {symbol.y=100;}
+    //let symbol = reel.getChildAt(0);
+    //symbol.y += 1;
+    renderer.render(reel);
+    //if (symbol.y>700) {symbol.y=10;}
 }
